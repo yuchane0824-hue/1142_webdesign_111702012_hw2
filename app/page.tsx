05465f2cf16614f5a7d6c0849e6128254865aba2
yuchane0824-hue <yuchane0824@gmail.com>
2026-04-30 14:00:55 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,37 @@
 "use client"
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
 
+
+    //開始頁面
+
+    //題目
+
+    //結果頁面
+
     const [counter, setCounter] = useState(0);
 
+    function addSleep(){
+      console.log("zzzz");
+      setCounter(counter+1);
+    }
+
+    useEffect(function(){
+      console.log("蛤")
+    },[counter]);
+
+    useEffect(function(){
+      console.log("隨便寫")
+    },[]);
+
     return(
-        <> 
-        好想睡 {counter} 覺
+      <>
+        <div className="bg-amber-100 w-[480px] h-screen flex justify-center items-center flex-col"> 
+          <div>開始遊戲 </div>
+          <div className="bg-black px-6 py-3">開始遊戲</div>
+        </div>
         </>
     )
 
